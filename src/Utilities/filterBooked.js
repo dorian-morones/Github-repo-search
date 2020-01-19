1933957e7667f0550d9deb9aef07e34e886777a8
Dorian Morones <dorianm393@gmail.com>
2020-01-19 13:15:08 -0600

    Filter Booked repo logic - updte

--- src/Utilities/filterBooked.js
+++ src/Utilities/filterBooked.js
@@ -3,7 +3,7 @@ import store from '../store/index.js';
 export const filterBooked = (keyword) => {
     let storedRepo = store.getState().results.storedRepo;
     console.log("keyword", keyword)
-    let filtered = storedRepo.filter(item => item.name === keyword)
+    let filtered = storedRepo.filter(item => item.name.toUpperCase().match(keyword.toUpperCase()))
 
     let result = keyword === '' || keyword === undefined ? storedRepo : filtered
     return result
